5afc2599e865e210b68cfe8c43d75bd9965f30eb
Almir Jr <99154364+AlmirJrDev@users.noreply.github.com>
2024-07-15 09:58:07 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Nunito',
+      },
+      backgroundImage: {
+        pattern: 'url(/Cover.png)'
+      },
+      colors: {
+        blue: "#3294F8",
+        baseTitle: "#E7EDF4",
+        baseSubtitle: "#C4D4E3",
+        baseText: "#AFC2D4",
+        baseSpan: "#7B96B2",
+        baseLabel: "#3A536B",
+        baseBorder: "#1C2F41",
+        basePost: "#112131",
+        baseProfile: "#0B1B2B",
+        baseBackground: "#071422",
+        baseInput: "#040F1A",
+      }
+    },
+  },
+  plugins: [],
+}
